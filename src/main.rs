63d1545dd2efa60194edb229826099a66ce426a4
Lev Kadatskyi <levkadatscky@gmail.com>
2025-12-16 10:28:24 +0200

    updated main.rs to use library

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use disktool::scan_dir;
 use humansize::{DECIMAL, format_size};
-use walkdir::WalkDir;
 
 #[derive(Parser)]
 struct Args {
@@ -11,44 +11,6 @@ struct Args {
     // Top heaviest files to show
     #[arg(long, default_value = "10")]
     top: usize,
-}
-
-#[derive(Debug)]
-struct FileInfo {
-    path: PathBuf,
-    size: u64,
-}
-
-#[derive(Debug)]
-struct DirectoryReport {
-    total_size: u64,
-    top_files: Vec<FileInfo>,
-}
-
-fn scan_dir(path: &PathBuf, top_n: usize) -> DirectoryReport {
-    let mut files = Vec::new();
-    let mut total_size = 0;
-
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        if entry.file_type().is_file() {
-            if let Ok(metadata) = entry.metadata() {
-                let size = metadata.len();
-                total_size += size;
-                files.push(FileInfo {
-                    path: entry.path().to_path_buf(),
-                    size,
-                });
-            }
-        }
-    }
-
-    files.sort_by_key(|f| std::cmp::Reverse(f.size));
-    let top_files = files.into_iter().take(top_n).collect();
-
-    DirectoryReport {
-        total_size,
-        top_files,
-    }
 }
 
 fn main() {
