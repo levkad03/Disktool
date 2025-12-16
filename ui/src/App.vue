b40d194b5a6f21fdd160a799ffc49b59ba2466a1
Lev Kadatskyi <levkadatscky@gmail.com>
2025-12-16 11:00:41 +0200

    created basic page for showing results of scanning

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+interface FileInfo {
+    path: string;
+    size: number;
+}
+
+interface DirectoryReport {
+    total_size: number;
+    top_files: FileInfo[];
+}
+
+const path = ref<string>("");
+const top = ref<number>(10);
+const report = ref<DirectoryReport | null>(null);
+
+async function scan() {
+    report.value = await invoke<DirectoryReport>("scan", {
+        path: path.value,
+        top: top.value,
+    });
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <h2>Disk Tool</h2>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <input v-model="path" placeholder="Path" />
+    <input type="number" v-model="top" />
+    <button @click="scan">Scan</button>
+
+    <pre v-if="report">{{ report }}</pre>
+</template>
